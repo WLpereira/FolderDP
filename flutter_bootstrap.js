cb1da128e8be6ec26b8fbc89c4fce43ee7311e7f
WLpereira <WLpereira@users.noreply.github.com>
2025-05-14 21:04:41 +0000

    deploy: e282ae5cb32b1552096955543f41442b07243923

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"38e19d01dcac654bbb941b771cc06efd4ed4a9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1432792798"
+    serviceWorkerVersion: "3924105822"
   }
 });
