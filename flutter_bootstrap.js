2e308915e51221ed066692c11e6bca3e8cea8c86
WLPereira <washingtton_@hotmail.com>
2025-05-14 15:54:01 -0300

    build: ajuste efeito

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2879687090"
+    serviceWorkerVersion: "4125420239"
   }
 });
